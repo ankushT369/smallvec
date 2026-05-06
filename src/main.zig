854c3c24fbc1f28040b3e18163d68651a772563e
Ankush <mondalankush9851@gmail.com>
2026-05-06 17:57:50 +0530

    shrink to fit support

--- src/main.zig
+++ src/main.zig
@@ -3,19 +3,33 @@ const sv = @import("smallvec");
 
 pub fn main() !void {
     const Vec = sv.SmallVec(u32, 64);
-    const N: usize = 1_000;
+    const N: usize = 75;
+    var iter: usize = 50;
 
     // by default it uses std.heap.smp_allocator
     var v = Vec.init(.{});
     defer v.deinit();
 
+    std.debug.print("before capacity: {}\n", .{v.capacity()});
+    std.debug.print("before len: {}\n", .{v.len()});
+
     for (0..N) |i| {
         try v.push(@intCast(i));
     }
 
-    while (!v.isEmpty()) {
+    std.debug.print("after capacity: {}\n", .{v.capacity()});
+    std.debug.print("after len: {}\n", .{v.len()});
+
+    while (!v.isEmpty() and iter > 0) {
         if (v.pop()) |value| {
-            std.debug.print("{}\n", .{value});
+            std.debug.print("{}", .{value});
         }
+
+        iter -= 1;
     }
+
+    v.shrinkToFit();
+
+    std.debug.print("after shrink capacity: {}\n", .{v.capacity()});
+    std.debug.print("after shrink len: {}\n", .{v.len()});
 }
